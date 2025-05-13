18089a3a35ab8e6801ca10461e2f09fce6885f96
Cesar Orlando Magaña Pasaye <cesarorlando@live.com.mx>
2025-05-13 08:49:06 -0600

    feat: cuando la IA esta desactivada

--- src/services/whatsapp/message.service.ts
+++ src/services/whatsapp/message.service.ts
@@ -1,4 +1,5 @@
 import { findOrCreateCustomer } from "../../controllers/record.controller";
+import Chat from "../../models/chat.model";
 import { handleMessageType } from "./messageTypeHandler.service";
 
 export const processMessage = async (body: any, io: any) => {
@@ -11,6 +12,23 @@ export const processMessage = async (body: any, io: any) => {
   // Si no se encuentra el cliente, no procesar el mensaje
   if (aiField?.value == "false") {
     console.log("🤖 El cliente tiene AI desactivada.");
+    let chat = await Chat.findOne({ phone: WaId });
+    if (!chat) {
+      chat = new Chat({
+        phone: WaId,
+        linkedTable: {
+          refModel: "DynamicRecord",
+          refId: customer._id,
+        },
+        conversationStart: new Date(),
+      });
+    }
+    chat.messages.push({
+      direction: "inbound",
+      body: Body,
+      respondedBy: "bot",
+    });
+    await chat.save();
     return { message: "El cliente tiene AI desactivada." };
   } else {
     // Procesar el mensaje utilizando handleMessageType
